890b4e0b18d36f455b81f4443ba0ec283f67c8f1
Daniellehawii <105246100+Daniellehawii@users.noreply.github.com>
2025-11-08 15:19:24 -0800

    Added comments to gmae and hpbar code

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,11 +1,20 @@
 extends Node
 
+#INITIALIZE PLAYER HEALTH
 var playerHP = 30
+#INITIALIZE BOSS HEALTH
 var bossHP = 30
 
+#INITIALIZING TRUE/FALSE BOOLEAN STATEMENTS AT GAME START
+#SET PLAYER DEATH TO FALSE
 var dead = false
+#SET CAMERA CHANGE/MOVE TO FALSE
 var camchange = false
+#SET PLAYER MOVEMENT TO TRUE
 var canmove = true
+#SET PLAYER GOT ITEM TO FALSE
 var got = false
+#SET SWORD TO TRUE
 var sword = true
+#SET BOSS TO FALSE
 var boss = false
