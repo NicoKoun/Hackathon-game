5de1798ac385e605df5ab3aee8c28a0e3b87a379
Morgan Griffiths <morgandgriffiths@icloud.com>
2025-11-08 17:22:48 -0800

    Added chest functionality

--- chest.gd
+++ chest.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
+var player_in_range = false
+var is_open = false
 
 func _ready():
 	get_node("AnimatedSprite2D").play("Idle")
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
-	if body.name == "player" and Input.is_action_just_pressed("Attack"):
-		open()
-			
+	if body.name == "player":
+		player_in_range = true
 
+func _on_player_detection_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		player_in_range = false
+		
+func _process(delta):
+	if player_in_range and Input.is_action_just_pressed("Attack"):
+		open()
 
 func open():
 	get_node("AnimatedSprite2D").play("Open")
 	var num_coins = randi_range(5, 20)
 	Game.coins += num_coins
+	is_open = false
 	
